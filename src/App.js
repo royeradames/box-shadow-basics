f7341eb5ede978ee11cd9b453a9f67cb0ac87fce
royeradames <royeraadames@gmail.com>
2021-02-05 12:59:42 -0500

    📦completed project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import 'normalize.css'
+import "./box-shadow-basics.scss"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <h1>
+        The <code>box-shadow</code> property
+      </h1>
     </div>
   );
 }
